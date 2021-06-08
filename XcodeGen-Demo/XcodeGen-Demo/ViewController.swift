26b14c91ff97d180fc8908ac9190203d5ba43631
[STV]Kazunori Aoki <k.aoki@stv-tech.co.jp>
2021-06-08 11:14:29 +0900

    XcodeGen-Demo

---
+++ XcodeGen-Demo/XcodeGen-Demo/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  XcodeGen-Demo
+//
+//  Created by Kazunori Aoki on 2021/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
